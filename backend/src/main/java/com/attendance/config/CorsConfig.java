921d8da03fa58f060e36bfaa2700117a60ef5046
deepi0903 <deepiksha0903@gmail.com>
2026-06-24 15:53:38 +0530

    feat(issue-1): complete initial project setup

---
+++ backend/src/main/java/com/attendance/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package com.attendance.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        // React Frontend URL
+        config.addAllowedOrigin("http://localhost:3000");
+
+        // Allow all headers
+        config.addAllowedHeader("*");
+
+        // Allow all HTTP methods
+        config.addAllowedMethod("*");
+
+        // Allow cookies/authentication
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
---
+++ src/test/java/com/attendance/employee_attendance_system/EmployeeAttendanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendance.employee_attendance_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAttendanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
